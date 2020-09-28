49b6e599e858b24553e99c8608a1398ae55835f3
MonadWizard <monad.wizard.r@gmail.com>
2020-09-29 00:49:27 +0600

    0.02

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.     makemigrations-> create model , migrate-> apply model
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=50, null=True)
+    email = models.EmailField(max_length=100, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Product(models.Model):
+
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True,choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out Of Stock', 'Out of Stock'),
+        ('Delivered', 'Delivered'),
+    )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True,choices=STATUS)
+
+
+    def __str__(self):
+        return self.status
+
+
